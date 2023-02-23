79ab6bb4a49b6010880180f5760f1b40651c0837
koelo <development.4n8f8@simplelogin.com>
2023-02-24 07:44:23 +0800

    Update ohio.py

--- ohio.py
+++ ohio.py
@@ -1,1 +1,14 @@
-  print("Ohio")
+import os
+import sys
+
+os.system("pkg install figlet")
+
+os.system("figlet ohio")
+time.sleep(2)
+print("Created by")
+time.sleep(1)
+os.system("figlet 1disk")
+time.sleep(1)
+print("Licensed under AGPL v3.0 license made by GNU")
+time.sleep(0.5)
+os.system("exit")
